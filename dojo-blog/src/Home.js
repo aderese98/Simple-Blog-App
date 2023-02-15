7b8f17728ce3e21cc4416ec43c43b3b3d42b7621
aderese98 <abrahamderese98@gmail.com>
2023-02-14 22:53:38 -0500

    Added bloglist component and updated home page

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-    // let name = "John";
-    const [likes, setLikes] = useState(0);
-    const handleClick = (e) => {
-        setLikes(likes+1);
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptatum eligendi possimus officiis vitae necessitatibus accusantium itaque autem optio, similique neque corporis reprehenderit maxime, cupiditate nemo eveniet provident nihil eius.", author: 'John Doe', id:1},
+        {title: 'blog 2', body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptatum eligendi possimus officiis vitae necessitatibus accusantium itaque autem optio, similique neque corporis reprehenderit maxime, cupiditate nemo eveniet provident nihil eius.", author: 'Jane Doe', id:2},
+        {title: 'blog 3', body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptatum eligendi possimus officiis vitae necessitatibus accusantium itaque autem optio, similique neque corporis reprehenderit maxime, cupiditate nemo eveniet provident nihil eius.", author: 'Mike Doe',id:3},
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     };
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ likes }</p>
-            <button onClick={handleClick}>Like</button>
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter(
+                (blog) => blog.author === "John Doe")} title="John's blogs"/>
         </div>
      );
 }
